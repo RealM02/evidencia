e25c45fbdecf3cf381b75351c649f40e1908f215
RealM02 <74264854+RealM02@users.noreply.github.com>
2023-04-10 18:27:39 -0600

    s

--- app/Http/Controllers/order_detailsController.php
+++ app/Http/Controllers/order_detailsController.php
@@ -10,7 +10,7 @@ class order_detailsController extends Controller
     public function detail(){
         $page_title="ORDER DETAILS";
         //select * from clients
-            $details=orders_details::all();
+            $details=order_details::all();
             return view('order_details', compact('page_title','details'));
         }
 }
--- app/Models/order_details.php
+++ app/Models/order_details.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class order_details extends Model
 {
     use HasFactory;
+
+    protected $table = "orders_details";
 }
